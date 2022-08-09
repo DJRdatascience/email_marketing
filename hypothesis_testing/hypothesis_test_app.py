4c98da09b3374dde094c57e8a93604e80a3a1874
DJRdatascience <daniel.j.rasmussen+ds@gmail.com>
2022-08-09 17:38:12 -0400

    updates

--- hypothesis_testing/hypothesis_test_app.py
+++ hypothesis_testing/hypothesis_test_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 from statsmodels.stats.power import TTestIndPower
-import statsmodels.stats.api as sms
+from statsmodels.stats.proportion import proportion_effectsize
 import plotly.express as px
 
 #####################################################################################
@@ -12,7 +12,7 @@ def calc_power(rate,power,alpha,lift=LIFT):
     nobs = [ [], [] ]
     analysis = TTestIndPower()
     for l in lift:
-        effect = sms.proportion_effectsize(rate, rate+l, method='normal')
+        effect = proportion_effectsize(rate, rate+l, method='normal')
         obs = analysis.solve_power(effect_size=effect, power=power, nobs1=None, ratio=1.0, alpha=alpha)
         if obs > 800:
             break
@@ -39,8 +39,12 @@ nobs = st.sidebar.number_input(
     'Recipients',value=200,min_value=25,max_value=800,step=1
 )
 
-RATE = st.sidebar.slider(
+OR = st.sidebar.slider(
     'Base open rate (%)', min_value=20, max_value=40, value=30, step=1
+)
+
+CR = st.sidebar.slider(
+    'Base click rate (%)', min_value=0.2, max_value=10, value=1, step=0.2
 )
 
 POWER = st.sidebar.slider(
@@ -63,15 +67,15 @@ st.markdown( '###' )
 # Row of plots
 #------------------------------------------------------------------------------------
 
-test = calc_power(RATE/100,POWER/100,ALPHA/100)
+test = calc_power(OR/100,POWER/100,ALPHA/100)
 
 #~~~~~~~~~~
 # Statistical power
 #~~~~~~~~~~
 
 fig1 = px.line(
-    x = test[0],
-    y = test[1],
+    x = np.array(test[0])[:,0],
+    y = np.array(test[1])[:,0],
     orientation='h',
     title='<b>Open Rates (percent)</b>',
     template='simple_white'
@@ -90,8 +94,8 @@ fig1.update_layout(
 #~~~~~~~~~~
 
 fig1 = px.line(
-    x = test[0],
-    y = test[1],
+    x = np.array(test[0])[:,0],
+    y = np.array(test[1])[:,0],
     orientation='h',
     title='<b>Open Rates (percent)</b>',
     template='simple_white'
@@ -110,8 +114,8 @@ fig1.update_layout(
 #~~~~~~~~~~
 
 fig1 = px.line(
-    x = test[0],
-    y = test[1],
+    x = np.array(test[0])[:,0],
+    y = np.array(test[1])[:,0],
     orientation='h',
     title='<b>Open Rates (percent)</b>',
     template='simple_white'
