5dafc04cd1ace8327da027ffecb7522066b58984
DJRdatascience <daniel.j.rasmussen+ds@gmail.com>
2022-08-16 14:10:20 -0400

    updates

--- hypothesis_testing/hypothesis_test_app.py
+++ hypothesis_testing/hypothesis_test_app.py
@@ -8,7 +8,7 @@ import plotly.express as px
 # FUNCTIONS
 #####################################################################################
 
-def calc_chipower( param_calc, *args ):
+def calc_chipower( param_calc, *args ): # Currently, the chi-squared test is not used
 
     # unpack arguments, last argument can be either lift (if param_calc=='Lift') or nobs (if param_calc=='Recipients')
     rate, alpha, power, last = args
